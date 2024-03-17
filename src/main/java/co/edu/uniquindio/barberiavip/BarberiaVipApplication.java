3a890d7ebb8c2bfd006fbe05fca203622e29e2d4
arcundio <santiagoarciniegasc@gmail.com>
2024-03-16 19:12:01 -0500

    Primer commit

---
+++ src/main/java/co/edu/uniquindio/barberiavip/BarberiaVipApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.barberiavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberiaVipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberiaVipApplication.class, args);
+	}
+
+}
